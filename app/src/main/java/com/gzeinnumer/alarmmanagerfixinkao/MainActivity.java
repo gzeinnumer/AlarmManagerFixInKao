ce3bf0f9c8de414616147b6b41fc13f7fa50c0cf
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-07-22 20:24:18 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/alarmmanagerfixinkao/MainActivity.java
@@ -0,0 +1,49 @@
+package com.gzeinnumer.alarmmanagerfixinkao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.gzeinnumer.alarmmanagerfixinkao.service.AlarmReceiver;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity_";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startAlarm();
+    }
+
+    private void startAlarm() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+
+        //TRIGGER Alaram Pada
+        calendar.set(Calendar.HOUR_OF_DAY,23);
+        calendar.set(Calendar.MINUTE, 12);
+//        calendar.set(Calendar.SECOND, 59);
+
+        AlarmManager alarmMgr = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(getApplicationContext(), AlarmReceiver.class);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (alarmMgr!=null){
+                alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, alarmIntent);
+            } else {
+                Log.d(TAG, "startAlarm: alarmMgr null");
+            }
+        }
+    }
+}
